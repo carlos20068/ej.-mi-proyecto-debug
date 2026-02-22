2cae04d5bb08c30f0e4b62a178fa67705072b78b
carlos20068 <crmelgars@alumno.uspg.edu.gt>
2026-02-22 14:34:08 -0600

    Fix syntax errors in calcularPromedio function
    hice unos cambios para que el bug se corrigiera

--- calculobug.py
+++ calculobug.py
@@ -8,3 +8,14 @@ function calcularPromedio(numeros){ # type: ignore
 let datos = [10, 20, 30, 40, 50];
 let resultado = calcularPromedio(datos);
 console.log("El promedio es: " , + resultado);
+# cambios de thebuging
+function calcularPromedio(numeros) {
+    lest suma = 0;
+flor (let i =0; i < numeros.length; i++) {
+suma += numero[i];
+}
+return suma / numero. length;
+}
+let datos = [10,20,30,40,50];
+let resultado = calcularPromedio(datos);
+console.log("El promedio es:", resultado);
