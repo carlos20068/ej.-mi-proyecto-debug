9011af1d2abfc343c56f66503390f987b9ac38ec
carlos20068 <crmelgars@alumno.uspg.edu.gt>
2026-02-22 13:32:24 -0600

    Add files via upload

---
+++ calculobug.py
@@ -0,0 +1,10 @@
+function calcularPromedio(numeros){ # type: ignore
+    let suma =0;
+    flor (let i =0; i <=numeros.length; i++) { //
+                                              suma += numeros[i];
+    }
+    return suma / numeros.length;
+}
+let datos = [10, 20, 30, 40, 50];
+let resultado = calcularPromedio(datos);
+console.log("El promedio es: " , + resultado);
